5ced63c38158217bdeb1a5157af292c79b61f51c
Slava Boss <prizman2000@mail.ru>
2020-12-23 00:04:10 +1000

    project depends to package progress

---
+++ index.js
@@ -0,0 +1,32 @@
+module.exports = require('./node_modules/progress/lib/node-progress');
+
+var ProgressBar = require('progress');
+var https = require('https');
+
+var req = https.request({
+    host: 'download.github.com',
+    port: 443,
+    path: '/visionmedia-node-jscoverage-0d4608a.zip'
+});
+
+req.on('response', function(res){
+    var len = parseInt(res.headers['content-length'], 10);
+
+    console.log();
+    var bar = new ProgressBar('  downloading [:bar] :rate/bps :percent :etas', {
+        complete: '=',
+        incomplete: ' ',
+        width: 20,
+        total: len
+    });
+
+    res.on('data', function (chunk) {
+        bar.tick(chunk.length);
+    });
+
+    res.on('end', function () {
+        console.log('\n');
+    });
+});
+
+req.end();
